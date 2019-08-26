42a588b633cfdd35f64ce794a4611576b002d569
coyet <coyet@VincentJunior.wireless-1x.unc.edu>
2019-08-25 23:49:16 -0400

    Initial testing

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,16 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Store the first integer in a variable
+		int count = scan.nextInt();
+		
+		// Make an array with # of cells based on first integer
+		int[] values = new int[count];
+		
+		// For loop to add values to the array
+		
+		// Print statement for testing
+		System.out.println(count);
+		
 	}
 }
