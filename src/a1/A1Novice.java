01732515eeddb566fc5908392f611e0658bf0cd5
coyet <coyet@VincentJunior.wireless-1x.unc.edu>
2019-08-26 19:36:20 -0400

    added unfinished for loop, testing to see if first initial prints

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,17 +9,23 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		// you shouldn't have to use any Scanner methods 
+		// other than next(), nextInt(), and nextDouble()
 		
 		// Store the first integer in a variable
-		int count = scan.nextInt();
+		int totalCustomers = scan.nextInt();
 		
-		// Make an array with # of cells based on first integer
-		int[] values = new int[count];
+		// For loop that loops every customer,
+		// inner for loop that loops every item
+		for(int n=0; n<totalCustomers; n++) {
+			 String firstName = scan.next();
+			 char firstInitial = firstName.charAt(0);
+			 System.out.println(firstInitial + ".");
+			}
 		
-		// For loop to add values to the array
 		
-		// Print statement for testing
-		System.out.println(count);
+		
+		
 		
 	}
 }
