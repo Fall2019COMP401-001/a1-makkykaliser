a0026136c9391fb84bf562ffa9dd5aa0e99a482b
coyet <coyet@VincentJunior.wireless-1x.unc.edu>
2019-08-30 19:47:55 -0400

    Temporary print statement will show me if my 2 arrays work correctly

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,30 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+
+		// Empty variables to store the biggest/smallest/average values
+		String biggest = "";
+		String smallest = "";
+		String average = "";
+		
+		// Variable to store the first integer
+		int totalItems = scan.nextInt();
+		
+		// Create array to store item names
+		String[] itemNames = new String[totalItems];
+		
+		// Create array to store prices
+		double[] itemPrices = new double[totalItems];
+		
+		// For loop to add items and prices to the arrays
+		for (int n = 0; n < totalItems; n++) {
+			itemNames[n] = scan.next();
+			itemPrices[n] = scan.nextInt();
+		}
+		
+		System.out.println(itemNames);
+		System.out.println(itemPrices);
+		
 		
 	}
 }
