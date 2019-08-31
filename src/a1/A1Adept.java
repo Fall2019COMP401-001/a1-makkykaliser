9cb1791e163a10e6261650152be1b15d87cdb7f9
coyet <coyet@VincentJunior.wireless-1x.unc.edu>
2019-08-30 20:21:28 -0400

    Temporary print statement shows if i calculated item prices correctly

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -30,8 +30,50 @@ public class A1Adept {
 			itemPrices[n] = scan.nextInt();
 		}
 		
-		System.out.println(itemNames);
-		System.out.println(itemPrices);
+		// Variable to store number of customers
+		int totalCustomers = scan.nextInt();
+		
+		// Array to store customer names
+		String[] customerNames = new String[totalCustomers];
+		
+		// Array to store customer totals
+		double[] customerTotals = new double[totalCustomers];
+		
+		// Nested for loops to add customers and totals to respective arrays
+		
+		// This for loop adds customer names to respective array and finds # of purchased items
+		for (int n = 0; n < totalCustomers; n++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			customerNames[n] = firstName + " " + lastName;
+			int purchasedItems = scan.nextInt();
+			double customerTotal = 0;
+			
+			// This for loop iterates for each product on customer's list, 
+			// finds price of item based on array index & multiplies by number of copies
+			for (int m = 0; m < purchasedItems; m++) {
+				double numberOfItem = scan.nextDouble();
+				// numberOfItem will get multiplied by price of item
+				String thisItem = scan.next();
+				int thisItemIndex = -1;
+				double thisItemPrice = 0;
+				
+				// This for loop iterates to find the index of each product
+				for (int p = 0; p < totalItems; p++) {
+					if (itemNames[p].equals(thisItem)) {
+						thisItemIndex = p;
+					}
+				}
+				// This code is inside customer total for loop
+				thisItemPrice = itemPrices[thisItemIndex];
+				thisItemPrice = thisItemPrice * numberOfItem;
+				
+				//temporary print statement
+				System.out.println(thisItemPrice);
+				
+			}
+			// This code is inside original for loop
+		}
 		
 		
 	}
