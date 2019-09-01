de9dc5f869af38aee19bbdc84b4b20542656de7d
coyet <coyet@VincentJunior.wireless-1x.unc.edu>
2019-09-01 14:08:14 -0400

    Changed the way currentCustomerForItem[] and currentCustomer are compared

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -67,7 +67,7 @@ public class A1Jedi {
 				for (int l = 0; l < totalItems; l++) {
 					if (itemNames[l].equals(thisItem)) {
 						itemCopies[l] = itemCopies[l] + numberOfItem;
-						if (!currentCustomerForItem[l].equals(currentCustomer)) {
+						if ((currentCustomerForItem[l].equals(currentCustomer)) == false) {
 							customersBought[l] = customersBought[l] + 1;
 						}
 						currentCustomerForItem[l] = currentCustomer;
